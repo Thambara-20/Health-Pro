718571dffca3a232fc42a4a1f35de165b4f5fd6a
Thambara Sahassaka <thambara.20@cse.mrt.ac.lk>
2023-09-18 15:40:30 +0530

    added some components

--- bmicalculator/lib/main_page.dart
+++ bmicalculator/lib/main_page.dart
@@ -11,86 +11,184 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 20, left: 20),
-                    child: const Text(
-                      'BMI Calculator',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                        
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 124, 243, 33),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          margin: const EdgeInsets.only(top: 20, left: 20),
-                          child: Column(
-                            children: const [
-                            Text(
-                              'Male',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Icon(
-                              Icons.male,
-                              size: 100,
-                            ),
-                          ])),
-                    ],
-                  ),
-                  Spacer(),
-                  Column(
-                    children: [
-                      Container(
-                          
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 33, 170, 243),
-                            borderRadius: BorderRadius.circular(10),
-                          ) 
-                          ,
-                          
-                          margin: const   EdgeInsets.only(
-                              top: 20, left: 20, right: 20),
-                          child: Column(children: const [
-                            Text(
-                              'Female',
-                              style: TextStyle(
-                                fontSize: 20, 
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Icon(
-                              Icons.female,
-                              size: 100,
-                            )
-                          ])),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+        appBar: AppBar(
+          title: Text(
+            'BMI Calculator',
+            style: TextStyle(color: Colors.black),
           ),
         ),
-      ),
-    );
+        body: SafeArea(
+          child: Container(
+            color: Color.fromARGB(255, 0, 0, 0),
+            child: Column(children: [
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                              color: Colors.green[500],
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0)),
+                          margin: const EdgeInsets.only(top: 20, left: 20),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(children: const [
+                              Text(
+                                'Male',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Icon(
+                                Icons.male,
+                                size: 100,
+                              ),
+                            ]),
+                          )),
+                    ],
+                  ),
+                  const Spacer(),
+                  Column(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                              color: Colors.yellow[600],
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0)),
+                          margin: const EdgeInsets.only(
+                              top: 20, left: 20, right: 20),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(children: const [
+                              Text(
+                                'Female',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Icon(
+                                Icons.female,
+                                size: 100,
+                              )
+                            ]),
+                          )),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 0, 208, 255),
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: Color.fromARGB(255, 255, 255,
+                                    255), // Set the border color here
+                                width: 2.0,
+                              )),
+                          margin: const EdgeInsets.only(top: 20, left: 20),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(children: [
+                              const Text(
+                                'Height',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const Text(
+                                '151',
+                                style: TextStyle(
+                                  fontSize: 60,
+                                  color: Color.fromARGB(255, 11, 66, 148),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                children: const [
+                                  FloatingActionButton(
+                                      onPressed: null,
+                                      backgroundColor: Colors.black,
+                                      child: Icon(Icons.add, size: 30)),
+                                  const SizedBox( width:10)
+                                ,
+                                  FloatingActionButton(
+                                    onPressed: null,
+                                    backgroundColor: Colors.black,
+                                    child: Icon(Icons.remove, size: 30),
+                                  )
+                                ],
+                              )
+                            ]),
+                          )),
+                    ],
+                  ),
+                  const Spacer(),
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 255, 143, 69),
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                width: 2.0)),
+                        margin:
+                            const EdgeInsets.only(top: 20, left: 20, right: 20),
+                        child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                const Text(
+                                  'weight',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  '11',
+                                  style: TextStyle(
+                                    fontSize: 60,
+                                    color: Color.fromARGB(255, 129, 0, 0),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                 Row(
+                                children: const [
+                                  FloatingActionButton(
+                                      onPressed: null,
+                                      backgroundColor: Colors.black,
+                                      child: Icon(Icons.add, size: 30)),
+                                  const SizedBox( width:10)
+                                ,
+                                  FloatingActionButton(
+                                    onPressed: null,
+                                    backgroundColor: Colors.black,
+                                    child: Icon(Icons.remove, size: 30),
+                                  )
+                                ],
+                              )
+                              ],
+                            )),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        ));
   }
 }
